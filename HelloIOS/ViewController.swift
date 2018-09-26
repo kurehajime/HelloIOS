744f511358504ee217b7dfc08c8fa9d09e206621
kurehajime <xiidec@gmail.com>
2018-09-26 23:24:00 +0900

    hello

--- HelloIOS/ViewController.swift
+++ HelloIOS/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tapToggle(_ sender: Any) {
+        if view.backgroundColor==UIColor.white{
+            view.backgroundColor=UIColor.green
+        }else{
+            view.backgroundColor=UIColor.white
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
